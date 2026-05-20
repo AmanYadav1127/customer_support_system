12c02eea9106897ab9158757a1ee4a72ad7845b7
Aman Yadav <aman.yadav.st.johns@gmail.com>
2026-05-21 00:19:20 +0530

    project setup(not fully done)

---
+++ backend/src/test/java/com/customersupport/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customersupport.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
